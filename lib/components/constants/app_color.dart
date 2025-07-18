1a075d174adc13e1a3b6c7d4470e268972457f8d
Dev7-Mubarak <mubark7382@gmail.com>
2025-07-18 13:58:04 -0700

    feat: Add ContextExtension for enhanced BuildContext functionality

--- lib/components/constants/app_color.dart
+++ lib/components/constants/app_color.dart
@@ -5,4 +5,6 @@ class AppColor {
   static const Color white = Color(0xFFFFFFFF);
   static const Color gray = Color(0xFFF3F4F6);
   static const Color gray2 = Color(0xFFE4E4E4);
+  static const Color success = Color(0xFF28A745);
+  static const Color danger = Color(0xFFDC3545);
 }
