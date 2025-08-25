6e4a334a616b9b6db457d2bdc8d9348f1498e45b
Reem <reemalthibani46@gmail.com>
2025-08-25 11:51:46 +0300

    fix login page

--- lib/features/auth/cubits/login_cubit/login_cubit.dart
+++ lib/features/auth/cubits/login_cubit/login_cubit.dart
@@ -28,6 +28,8 @@ class LoginCubit extends Cubit<LoginState> {
       );
       userData = UserData.fromJson(response["data"]);
       CacheHelper().saveData(key: 'id', value: userData.id);
+      CacheHelper().saveData(key: 'token', value: userData.token);
+      CacheHelper().saveData(key: 'email', value: userData.email);
       emit(LoginSuccess(userData));
     } on Serverexception catch (e) {
       emit(LoginFailure(errorMessage: e.errModel.errorMessage));
--- lib/features/auth/data/models/login_model.dart
+++ lib/features/auth/data/models/login_model.dart
@@ -1,11 +1,14 @@
 class UserData {
   late String id;
   late String email;
+  late String token;
+  
 
-  UserData({required this.id, required this.email});
+  UserData({required this.id, required this.email,required this.token});
 
   UserData.fromJson(Map<String, dynamic> json) {
     id = json['id'] ?? '';
     email = json['email'] ?? '';
+    token = json['token'] ?? '';
   }
 }
