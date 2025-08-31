4d20c9ea58322978f719489874df27a95181ba96
Reem <reemalthibani46@gmail.com>
2025-09-01 00:06:49 +0300

    add ContextExtension method to pages

--- lib/features/Assistances/presentation/views/add_family_to_assistanc.dart
+++ lib/features/Assistances/presentation/views/add_family_to_assistanc.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:smart_negborhood_app/core/constants/app_color.dart';
 import 'package:smart_negborhood_app/core/constants/app_route.dart';
 import 'package:smart_negborhood_app/core/common/widgets/smallButton.dart';
+import 'package:smart_negborhood_app/core/extensions/context_extension.dart';
 import 'package:smart_negborhood_app/features/annoucements/cubits/assistances/assistances_cubit.dart';
 import 'package:smart_negborhood_app/features/annoucements/cubits/assistances/assistances_state.dart';
 import 'package:smart_negborhood_app/features/families/cubits/family_cubit/family_cubit.dart';
@@ -39,31 +40,14 @@ class AddFamilyToAssistanceState extends State<AddFamilyToAssistance> {
     return BlocListener<AssistancesCubit, AssistancesState>(
       listener: (context, state) {
         if (state is WiateAssignFamilyToAssistance) {
-          showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (context) => const PopScope(
-              canPop: false,
-              child: Center(child: CircularProgressIndicator()),
-            ),
-          );
+          context.showLoadingDialog();
         } else if (state is FamilyAssignedSuccessfully) {
           Navigator.of(context, rootNavigator: true).pop();
           Navigator.of(context).pop();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(state.message),
-              backgroundColor: Colors.green,
-            ),
-          );
+          context.showSuccessSnackBar(state.message);
         } else if (state is AssistancesFailure) {
           Navigator.of(context, rootNavigator: true).pop();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(state.errorMessage),
-              backgroundColor: Colors.red,
-            ),
-          );
+          context.showErrorSnackBar(state.errorMessage);
         }
       },
       child: Scaffold(
