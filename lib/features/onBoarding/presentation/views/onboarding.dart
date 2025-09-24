4bad76b285095b14f4cd5f0cdee5c2872c4fe1c1
Dev7-Mubarak <mubark7382@gmail.com>
2025-09-24 15:57:28 +0300

    Refactor onboarding view: enhance layout structure, adjust padding, and improve text alignment for better UI presentation.

--- lib/features/onBoarding/presentation/views/onboarding.dart
+++ lib/features/onBoarding/presentation/views/onboarding.dart
@@ -11,59 +11,61 @@ class Onboarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: AppColor.primaryColor,
-        child: Column(
-          children: [
-            const Image(image: AssetImage(AppImage.onBoardingImage)),
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: const [
-                    CircularLogo(),
-                    SizedBox(height: 16),
-                    Text(
-                      'الحارة الذكية',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.w400,
-                        height: 26 / 40,
+      body: SafeArea(
+        child: Container(
+          color: AppColor.primaryColor,
+          child: Column(
+            children: [
+              const Image(image: AssetImage(AppImage.onBoardingImage)),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      CircularLogo(),
+                      SizedBox(height: 16),
+                      Text(
+                        'الحارة الذكية',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: AppColor.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.w400,
+                          height: 26 / 40,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    Text(
-                      'أدواتك لتنظيم الحي\nوالمربعات السكنية بكفاءة',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400,
-                        height: 31 / 25,
+                      SizedBox(height: 30),
+                      Text(
+                        'أدواتك لتنظيم الحي\nوالمربعات السكنية بكفاءة',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: AppColor.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          height: 31 / 25,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: DefaultButton(
-                text: 'تسجيل الدخول',
-                backgroundColor: AppColor.white,
-                color: AppColor.primaryColor,
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, AppRoute.login);
-                },
-                fontsize: 25,
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 40),
+                child: DefaultButton(
+                  text: 'تسجيل الدخول',
+                  backgroundColor: AppColor.white,
+                  color: AppColor.primaryColor,
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, AppRoute.login);
+                  },
+                  fontsize: 25,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
