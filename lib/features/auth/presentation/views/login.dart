641677c65fadcef280282c61363e641d592d01b8
Mubarak Bamazahem <mubark7382@gmail.com>
2025-09-25 10:12:07 +0300

    feat: integrate Sentry for error tracking and add necessary plugin registrations

--- lib/features/auth/presentation/views/login.dart
+++ lib/features/auth/presentation/views/login.dart
@@ -11,8 +11,6 @@ import 'package:smart_negborhood_app/core/utils/app_validator.dart';
 import 'package:smart_negborhood_app/features/auth/cubits/login_cubit/login_cubit.dart';
 import 'package:smart_negborhood_app/features/auth/cubits/login_cubit/login_state.dart';
 
-import '../../../../core/services/shared_preferences_service.dart';
-
 class Login extends StatefulWidget {
   Login({super.key});
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:smart_negborhood_app/core/constants/app_route.dart';
 import 'package:smart_negborhood_app/core/services/shared_preferences_service.dart';
 import 'core/config/app_Bloc_observer.dart';
 import 'smart_neighbourhood_app.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferencesService.init();
   Bloc.observer = AppBlocObserver();
@@ -19,7 +21,23 @@ void main() async {
     initialRoute = AppRoute.mainHome;
   }
 
-  runApp(
-    SmartNeighbourhoodApp(appRouter: AppRouter(), initialRoute: initialRoute),
-  );
+  if (kReleaseMode) {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://a626d5df45d9e9dea0cb80dea84abffb@o4509708628525056.ingest.us.sentry.io/4510078787911680';
+        options.sendDefaultPii = true;
+      },
+      appRunner: () => runApp(
+        SmartNeighbourhoodApp(
+          appRouter: AppRouter(),
+          initialRoute: initialRoute,
+        ),
+      ),
+    );
+  } else {
+    runApp(
+      SmartNeighbourhoodApp(appRouter: AppRouter(), initialRoute: initialRoute),
+    );
+  }
 }
